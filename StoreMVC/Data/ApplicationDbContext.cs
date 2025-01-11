799bae847923c791b2f26479828cad2ddffb609c
simplykapsel <kapsel.contact@gmail.com>
2025-01-11 20:35:34 +0100

    Add project files.

---
+++ StoreMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StoreMVC.Models;
+
+namespace StoreMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
